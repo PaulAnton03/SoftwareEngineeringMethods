33414df43d9c69579c06816b0aea020ad084437d
Ada Turgut <a.turgut@student.tudelft.nl>
2023-12-11 22:00:46 +0100

    started with the issue outline

--- example-microservice/src/main/java/nl/tudelft/sem/template/example/domain/order/OrderService.java
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/domain/order/OrderService.java
@@ -1,7 +1,23 @@
 package nl.tudelft.sem.template.example.domain.order;
 
+import nl.tudelft.sem.template.model.Location;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class OrderService {
+
+    public OrderRepository orderRepo;
+
+
+    public OrderService(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+
+    public Location getFinalDestinationOfOrder(BigDecimal orderId){
+//        orderRepo.getOne(orderId);
+        return null;
+    }
 }
