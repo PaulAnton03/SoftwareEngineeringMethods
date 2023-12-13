d9c29b7bec5da5b1bc03bb8d29a02c5b0e45cae2
Klára Hirmanová <k.hirmanova@student.tudelft.nl>
2023-12-14 00:25:09 +0100

    implement StatusService with method for updateToAccept

--- example-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/StatusController.java
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/StatusController.java
@@ -1,11 +1,41 @@
 package nl.tudelft.sem.template.example.controllers;
 
 import nl.tudelft.sem.template.api.StatusApi;
+import nl.tudelft.sem.template.example.domain.order.StatusService;
+import nl.tudelft.sem.template.model.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/status")
 public class StatusController implements StatusApi {
+
+    public StatusService statusService;
+
+    public StatusController(StatusService statusService) {
+        this.statusService = statusService;
+    }
+
+    /**
+     * Handles put request for (/status/{orderId}/accepted).
+     *
+     * @param authorization The userId to check if they have the rights to make this request (required)
+     * @param orderId id of the order to update its status to accepted (required)
+     * @return a response entity with nothing, 404 if not found  403 if not authorized, only for vendors
+     */
+
     @Override
     public ResponseEntity<Void> updateToAccepted(Long orderId, Long authorization) {
+
+        Optional<Order> order = statusService.updateStatusToAccepted(orderId);
+
+        if(order.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
         return StatusApi.super.updateToAccepted(orderId, authorization);
     }
 }
--- example-microservice/src/main/java/nl/tudelft/sem/template/example/domain/order/StatusService.java
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/domain/order/StatusService.java
@@ -1,7 +1,36 @@
 package nl.tudelft.sem.template.example.domain.order;
 
+import nl.tudelft.sem.template.model.Order;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class StatusService {
+
+
+    public OrderRepository orderRepo;
+
+
+    public StatusService(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+
+    /**
+     * Attempts to update the status of order to accepted
+     * Vendors use this
+     *
+     * @param orderId the id of the order
+     * @return the optional of updated order object, empty if the order was not found
+     */
+    public Optional<Order> updateStatusToAccepted(Long orderId) {
+        try {
+            Order o = orderRepo.getOne(orderId);
+            o.setStatus(Order.StatusEnum.ACCEPTED);
+            return Optional.of(orderRepo.save(o));
+        } catch (javax.persistence.EntityNotFoundException e) {
+            return Optional.empty();
+        }
+    }
+
 }
