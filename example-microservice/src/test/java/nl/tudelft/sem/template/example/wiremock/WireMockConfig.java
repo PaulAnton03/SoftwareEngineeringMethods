01be866efe8c53af64474d210658e486497ed85c
PaulAnton <p.anton@student.tudelft.nl>
2023-12-14 23:12:23 +0100

    AuthorizationServiceClass

--- example-microservice/src/test/java/nl/tudelft/sem/template/example/wiremock/WireMockConfig.java
+++ example-microservice/src/test/java/nl/tudelft/sem/template/example/wiremock/WireMockConfig.java
@@ -1,7 +1,12 @@
 package nl.tudelft.sem.template.example.wiremock;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
+
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import org.springframework.http.MediaType;
 
 /**
  * Utility class for managing WireMock server instances.
@@ -20,7 +25,7 @@ public class WireMockConfig {
      */
     public static void startUserServer() {
         if (userMicroservice == null || !userMicroservice.isRunning()) {
-            userMicroservice = new WireMockServer(WireMockConfiguration.options().port(USER_SERVER_PORT).dynamicPort());
+            userMicroservice = new WireMockServer(WireMockConfiguration.options().port(USER_SERVER_PORT));
             userMicroservice.start();
         }
     }
@@ -31,7 +36,7 @@ public class WireMockConfig {
      */
     public static void startOrderServer() {
         if (orderMicroservice == null || !orderMicroservice.isRunning()) {
-            orderMicroservice = new WireMockServer(WireMockConfiguration.options().port(ORDER_SERVER_PORT).dynamicPort());
+            orderMicroservice = new WireMockServer(WireMockConfiguration.options().port(ORDER_SERVER_PORT));
             orderMicroservice.start();
         }
     }
@@ -54,6 +59,18 @@ public class WireMockConfig {
         if (orderMicroservice != null && orderMicroservice.isRunning()) {
             orderMicroservice.stop();
         }
+    }
+
+    /**
+     * Sets up a WireMock stub for simulating a successful authorization.
+     * Use this in test classes to ignore the authorization.
+     */
+    public static void ignoreAuthorization() {
+        userMicroservice.stubFor(WireMock.get(urlPathMatching(("/user/[0-9]+/type")))
+            .willReturn(aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                .withBody("admin")));
     }
 
 }
