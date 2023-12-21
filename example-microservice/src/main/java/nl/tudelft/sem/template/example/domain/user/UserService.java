3bb5700e6554945b425693433c8a2099bfee3676
Michael Leandro da Rocha Frota Fokkens <M.L.daRochaFrotaFokkens@student.tudelft.nl>
2023-12-21 16:25:59 +0100

    generated javaDoc

--- example-microservice/src/main/java/nl/tudelft/sem/template/example/domain/user/UserService.java
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/domain/user/UserService.java
@@ -1,7 +1,16 @@
 package nl.tudelft.sem.template.example.domain.user;
 
+import nl.tudelft.sem.template.example.domain.order.OrderRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private CourierRepository courierRepo;
+
+    public UserService(CourierRepository courierRepo) {
+        this.courierRepo = courierRepo;
+    }
+
+
 }
