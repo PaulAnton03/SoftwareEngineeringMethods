5b5c87f72c98e23994adcdf179b6aadafe770b0e
Ada Turgut <a.turgut@student.tudelft.nl>
2024-01-14 10:35:44 +0100

    fix checkstyle

--- example-microservice/src/test/java/nl/tudelft/sem/template/example/controllers/AdminControllerTest.java
+++ example-microservice/src/test/java/nl/tudelft/sem/template/example/controllers/AdminControllerTest.java
@@ -7,11 +7,6 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 import nl.tudelft.sem.template.example.authorization.AuthorizationService;
 import nl.tudelft.sem.template.example.domain.admin.AdminService;
 import nl.tudelft.sem.template.model.DeliveryException;
