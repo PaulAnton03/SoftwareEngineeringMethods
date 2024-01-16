e985a4967850da6d152b3252477c042c2c7e5f9b
Klára Hirmanová <k.hirmanova@student.tudelft.nl>
2024-01-16 15:00:47 +0100

    vendor methods

--- example-microservice/src/main/java/nl/tudelft/sem/template/example/domain/user/VendorService.java
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/domain/user/VendorService.java
@@ -1,6 +1,9 @@
 package nl.tudelft.sem.template.example.domain.user;
 
+import nl.tudelft.sem.template.model.Vendor;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 public class VendorService {
 
@@ -9,5 +12,80 @@ public class VendorService {
     @Autowired
     public VendorService(VendorRepository vendorRepo) {
         this.vendorRepo = vendorRepo;
+    }
+
+    /**
+     * Tries to add the given vendor to the database
+     *
+     * @param vendor the vendor to add
+     * @return an optional of the added vendor (or empty optional)
+     */
+    public Optional<Vendor> makeVendor(Vendor vendor) {
+        Vendor saved = vendorRepo.saveAndFlush(vendor);
+        return Optional.of(saved);
+    }
+
+    /**
+     * Tries to add a new vendor with given id to the database
+     *
+     * @param vendorId the id to create a vendor with
+     * @return an optional of the added vendor (or empty optional)
+     */
+    public Optional<Vendor> makeVendorById(Long vendorId) {
+        Vendor vendor = new Vendor().id(vendorId);
+        Vendor saved = vendorRepo.saveAndFlush(vendor);
+        return Optional.of(saved);
+    }
+
+    /**
+     * Check if id of vendor exists
+     *
+     * @param id id of vendor to check
+     * @return boolean true if vendor with this id exists
+     */
+    public boolean existsVendor(Long id) {
+        return vendorRepo.existsById(id);
+    }
+
+    /**
+     * Gets the radius of vendor
+     * @param id id of vendor
+     * @return Optional of Double - radius
+     */
+    public Optional<Double> getRadiusOfVendor(Long id) {
+        Optional<Vendor> vendor = vendorRepo.findById(id);
+
+        if (vendor.isEmpty()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(vendor.get().getRadius());
+    }
+
+    /**
+     * Updates the radius of vendor
+     * @param id id of vendor
+     * @param body the new radius
+     * @return Optional of Double - new radius
+     */
+    public Optional<Double> updateRadiusOfVendor(Long id, Double body) {
+        Optional<Vendor> vendor = vendorRepo.findById(id);
+
+        if (vendor.isEmpty()) {
+            return Optional.empty();
+        }
+
+        vendor.get().setRadius(body);
+
+        return Optional.of(vendorRepo.saveAndFlush(vendor.get()).getRadius());
+    }
+
+    /**
+     * Gets vendor
+     * @param id id of vendor to get
+     * @return Optional of Vendor
+     */
+    public Optional<Vendor> getVendor(Long id) {
+        return vendorRepo.findById(id);
     }
 }
